7aed2073b538bbd77d6595d328bf8854a3cccc34
ZakariaBAtty <zbatty0912@gmail.com>
2022-03-27 16:05:10 +0000

    add model chnage passsowrd

--- src/views/account/Accountsecurity.js
+++ src/views/account/Accountsecurity.js
@@ -14,8 +14,58 @@ const Accountsecurity = () => {
           >
             <div style={{ padding: '20px' }}>
               <div>
-                <div class="btn-extension mr-1">Change Password</div>
+                <button
+                  type="button"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                  class="btn-extension mr-1"
+                >
+                  Change Password
+                </button>
               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header d-flex">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Change password
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <div class="form-group" style={{ marginBottom: '20px' }}>
+                <label>Current Password</label>
+                <input class="form-control" type="password" />
+              </div>
+              <div class="form-group" style={{ marginBottom: '20px' }}>
+                <label>New Password</label>
+                <input class="form-control" type="password" />
+              </div>
+              <div class="form-group" style={{ marginBottom: '20px' }}>
+                <label>Confirm Password</label>
+                <input class="form-control" type="password" />
+              </div>
+              <div
+                style={{ color: 'rgb(128, 0, 0)', margin: '10px 0px' }}
+              ></div>
+            </div>
+            <div class="modal-footer bo-top-border">
+              <button class="btn-black">Okay</button>
             </div>
           </div>
         </div>
